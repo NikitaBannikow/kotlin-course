30afe1209a2de06f25932db2f41ed967a413a9c1
NikitaBannikow <niikitaban@gmail.com>
2021-12-13 23:14:09 +0300

    Initial commit for Practical work 1

---
+++ src/main/kotlin/practical_work1/MainPracticalWork1.kt
@@ -0,0 +1,30 @@
+package practical_work1
+
+const val inputText =
+    "Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference.\n" +
+            "Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard " +
+            "library depends on the Java Class Library, but type inference allows its syntax to be more concise. \n" +
+            "Kotlin mainly targets the JVM, but also compiles to JavaScript (e.g., for frontend web applications " +
+            "using React) or native code (via LLVM); e.g., for native iOS apps sharing business logic with Android " +
+            "apps. Language development costs are borne by JetBrains, while the Kotlin Foundation protects the " +
+            "Kotlin trademark.";
+
+@Suppress("UNUSED_PARAMETER")
+fun main(args: Array<String>) {
+    println("Original text:")
+    println("==============")
+    println(inputText)
+    println()
+
+    makeFormatAndPrint(30, Alignment.LEFT)
+    makeFormatAndPrint(40, Alignment.RIGHT)
+    makeFormatAndPrint(35, Alignment.CENTER)
+    makeFormatAndPrint(60, Alignment.JUSTIFY)
+}
+
+fun makeFormatAndPrint(width:Int, alignment: Alignment) {
+    println("Formatted text (width:$width, alignment: $alignment)")
+    println("===========================================================")
+    println(alignText(inputText, width, alignment))
+    println()
+}
---
+++ src/main/kotlin/practical_work1/TextFormatter.kt
@@ -0,0 +1,136 @@
+package practical_work1
+
+import kotlin.text.StringBuilder as Text
+
+enum class Alignment {
+    LEFT,
+    RIGHT,
+    CENTER,
+    JUSTIFY
+}
+
+fun alignText(
+    originalText: String,
+    lineWidth: Int = 120,
+    alignment: Alignment = Alignment.LEFT
+): String {
+    if (originalText.isEmpty()) return ""
+
+    val notEmpty = { text: Text -> text.isNotEmpty() }
+
+    // Align a line
+    val align = fun(line: Text): Text {
+        val trimmed = Text(line.replace("\\s+".toRegex(), " ").trim())
+        return when (alignment) {
+            Alignment.RIGHT -> alignRight(trimmed, lineWidth)
+            Alignment.CENTER -> alignCenter(trimmed, lineWidth)
+            Alignment.JUSTIFY -> alignJustify(trimmed, lineWidth)
+            else -> alignLeft(trimmed, lineWidth)
+        }
+    }
+
+    // Add a word to a line and to a text if required
+    fun addWord(word: Text, line: Text, text: Text, toAppend: CharSequence = "") {
+        val len = line.length + word.length
+        if (len <= lineWidth) {
+            line.append(word).append(toAppend)
+            word.clear()
+        } else {
+            if (notEmpty(line)) text.append(align(line)).append('\n')
+            line.clear()
+            if (word.length <= lineWidth) {
+                line.append(word).append(toAppend)
+                word.clear()
+            } else {
+                text.append(word.subSequence(0, lineWidth)).append('\n')
+                val rest = word.subSequence(lineWidth, word.length)
+                word.clear().append(rest)
+                addWord(word, line, text, toAppend)
+            }
+        }
+    }
+
+    val text = Text()
+    val line = Text()
+    val word = Text()
+
+    for (char in originalText) {
+        when (char) {
+            '\n' -> {
+                if (notEmpty(word)) {
+                    addWord(word, line, text)
+                }
+                if (notEmpty(line)) {
+                    text.append(align(line))
+                    line.clear()
+                }
+                text.append('\n')
+            }
+            ' ', '\t' -> {
+                if (notEmpty(word)) {
+                    addWord(word, line, text, " ")
+                }
+            }
+            else -> word.append(char)
+        }
+    }
+
+    if (notEmpty(word)) addWord(word, line, text)
+    if (notEmpty(line)) text.append(align(line))
+
+    return text.toString()
+}
+
+@Suppress("UNUSED_PARAMETER")
+private fun alignLeft(line: Text, lineWidth: Int): Text {
+    // Already aligned to left
+    return line
+}
+
+private fun alignRight(line: Text, lineWidth: Int): Text {
+    return Text(line.padStart(lineWidth, ' '))
+}
+
+private fun alignCenter(line: Text, lineWidth: Int): Text {
+    return Text(line.padStart(line.length + (lineWidth - line.length) / 2 , ' '))
+}
+
+private fun alignJustify(line: Text, lineWidth: Int): Text {
+    // Check if we can justify
+    if (line.count { it == ' ' } == 0) return line
+
+    // Check how many spaces can be added
+    var spacesToAdd = lineWidth - line.length
+    if (spacesToAdd == 0) return line
+
+    // Split words
+    val words = line.split(" ")
+
+    // Spread whitespaces between words from begining and end
+    val spacesSpread = Array(words.size - 1) { 1 }
+
+    var forwardIndex = 0
+    var backwardIndex = spacesSpread.size - 1
+    var forward = true
+    while (spacesToAdd > 0) {
+        if (forward) {
+            spacesSpread[forwardIndex++]++
+            if (forwardIndex == spacesSpread.size) forwardIndex = 0
+        } else {
+            spacesSpread[backwardIndex--]++
+            if (backwardIndex < 0) backwardIndex = spacesSpread.size - 1
+        }
+        forward = !forward
+        spacesToAdd--
+    }
+
+    val result = Text()
+    for ((index, word) in words.withIndex()) {
+        result.append(word)
+        if (index < spacesSpread.size) {
+            result.append(" ".repeat(spacesSpread[index]))
+        }
+    }
+
+    return result
+}
